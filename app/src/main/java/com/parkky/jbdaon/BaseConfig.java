0ad22dcacd62eebed118c517289e59224b131289
Pkyley1992 <pkyley1992@gmail.com>
2023-09-16 16:21:41 +0900

    base modify

--- app/src/main/java/com/parkky/jbdaon/BaseConfig.java
+++ app/src/main/java/com/parkky/jbdaon/BaseConfig.java
@@ -10,17 +10,12 @@ public class BaseConfig extends AppCompatActivity {
 
     // 동작 테스트를 위한 주소 구성
     //public static String Server_Url = "http://cug.knowhow.or.kr/jjokji/";
-
     // 실제 운영을 위한 주소 구성
     public static String Server_Url = "http://www.jbdaon.com/main/";
 
     public static String Info_ListUrl = Server_Url + "info_list_json.php";
     public static String Info_ViewUrl = Server_Url + "info_view_json.php";
-//    public static String Car_Login_Url = "http://192.168.100.113/Web_Service_Test/Car_login_check.php";
-//    public static String Car_ReLogin_Url = "http://192.168.100.113/Web_Service_Test/Car_relogin_check.php";
-//    public static String Oil_ListUrl = "http://192.168.100.113/Web_Service_Test/Car_Oil_List.php";
-//    public static String Car_WorkOn_listUrl = "http://192.168.100.113/Web_Service_Test/Car_WorkOn_List.php";
-//    public static String Car_WorkOn_execUrl = "http://192.168.100.113/Web_Service_Test/Car_WorkOn_check.php";
+
 
     private static Handler handler = new Handler();
     public static void Print_Log(String TAG, String data) {
